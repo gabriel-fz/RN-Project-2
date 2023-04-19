915d12d70790757a2084f6bc103145836d4d5fbc
gabriel-fz <gabrielfzancanela@gmail.com>
2023-04-18 23:41:09 -0300

    adicionando a biblioteca de icones e melhorando header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
-import { Container, Logo } from './styles';
-
 import logoImg from '@assets/logo.png';
 
-export function Header () {
+import { Container, Logo, BackIcon, BackButton } from './styles';
+
+interface HeaderProps {
+  showBackButton?: boolean;
+}
+
+export function Header ({ showBackButton = false }: HeaderProps) {
   return (
     <Container>
+      {
+        showBackButton && (
+          <BackButton>
+            <BackIcon />
+          </BackButton>
+        )
+      }
+
       <Logo source={logoImg} />
     </Container>
   )
